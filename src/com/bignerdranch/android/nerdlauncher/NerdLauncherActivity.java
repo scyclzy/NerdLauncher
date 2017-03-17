c7f5122382affb705057b1ba9031d93c92b37d3d
lizhongyuan <scyclzy@gmail.com>
2017-03-17 20:32:23 +0800

    create NerdLauncher project

---
+++ src/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		
+		return new NerdLauncherFragment();
+	}
+}
